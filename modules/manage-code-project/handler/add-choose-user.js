e0b997d32cb2abc01e6a7c3b9c988c2c11644d6d
Benjamin Paothatat <bpaothat@asu.edu>
2017-04-19 20:34:34 -0700

    Included Comments

--- modules/manage-code-project/handler/add-choose-user.js
+++ modules/manage-code-project/handler/add-choose-user.js
@@ -15,6 +15,12 @@ const Users = require('../../user/model/user');
   * @returns {Null} responds with HTML page
   */
 function chooseUserAdd (request, reply) {
+    /**
+     * Presents a view for a user to choose an user
+     * @param {Projects} projects - All projects in Nicest
+     * @param {Courses} courses - All courses in Nicest
+     * @returns {Null} responds with HTML page that allows user to choose course that student will be added
+     */
     function presentView (team, users) {
         const usersInTeam = [];
 
@@ -31,6 +37,11 @@ function chooseUserAdd (request, reply) {
         reply.view('modules/manage-code-project/view/add-choose-user', viewInfo);
     }
 
+    /**
+     * Gets the users in the team
+     * @param {Object} -team object
+     * @returns {Null} calls getTeam function passing the team and all users
+     */
     function getUsers (team) {
         Users
             .find({})
@@ -39,6 +50,11 @@ function chooseUserAdd (request, reply) {
             });
     }
 
+    /**
+     * Gets the team for project
+     * @param {Object} -teamId argument to query Team collection to get team associated with project
+     * @returns {Null} calls getTeam function
+     */
     function getTeam (teamId) {
         Teams
             .find(teamId)
@@ -47,6 +63,10 @@ function chooseUserAdd (request, reply) {
             });
     }
 
+    /**
+     * Gets the project that was choosen
+     * @returns {Null} calls getTeam function
+     */
     function getProject () {
         Projects
             .find(request.query)
