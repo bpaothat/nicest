ba8317e855a2236d55cb2b66fb5d18c11f7fd216
Benjamin Paothatat <bpaothat@asu.edu>
2017-04-19 20:34:18 -0700

    Included Comments

--- modules/manage-code-project/handler/add-choose-team.js
+++ modules/manage-code-project/handler/add-choose-team.js
@@ -5,7 +5,6 @@
  */
 
 const Projects = require('../../code-project/model/project');
-
  /**
   * Allows user to select team to add user to
   * @param {Request} request - Hapi request
@@ -13,12 +12,23 @@ const Projects = require('../../code-project/model/project');
   * @returns {Null} responds with HTML page
   */
 function chooseUserTeamAdd (request, reply) {
+    /**
+     * Presents a view for a user to choose a team
+     * @param {Projects} projects - projects that are associated with the course
+     * @returns {Null} responds with HTML page that allows user to choose a team that student will be added
+     */
     function presentView (projects) {
-        const viewInfo = {projects: projects};
+        const viewInfo = {
+            projects: projects
+        };
 
         reply.view('modules/manage-code-project/view/add-choose-team', viewInfo);
     }
 
+    /**
+     * Gets all projects associated with choosen course in Nicest
+     * @returns {Null} calls getCourses function
+     */
     function getProjects () {
         Projects
             .find(request.query)
